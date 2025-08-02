29dbf26abbb488597bd96648b0e65dddcd0b3f51
Pranto78 <fshahriyar44@gmail.com>
2025-08-02 13:27:03 +0600

    Added the task 3 & 4

---
+++ Lab task_4/class_dom.js
@@ -0,0 +1,62 @@
+
+let count = 0;
+function increment() {
+  count++;
+  document.getElementById("count").innerText = count;
+}
+function decrement() {
+  count--;
+  document.getElementById("count").innerText = count;
+}
+
+// BMI Calculator
+function calculateBMI() {
+  const height = parseFloat(document.getElementById("height").value);
+  const weight = parseFloat(document.getElementById("weight").value);
+  const result = document.getElementById("bmiResult");
+
+  if (!height || !weight) {
+    result.innerText = "Please enter both height and weight.";
+    return;
+  }
+
+  const bmi = weight / ((height / 100) ** 2);
+  result.innerText = `Your BMI is ${bmi.toFixed(2)}`;
+}
+
+
+function changeTextColor() {
+  const color = document.getElementById("colorInput").value;
+  const box = document.getElementById("colorBox");
+  box.style.color = color;
+}
+
+
+function validateForm() {
+  const password = document.getElementById("password").value;
+  const confirm = document.getElementById("confirmPassword").value;
+  const msg = document.getElementById("strengthMsg");
+
+  if (password.length < 8) {
+    msg.textContent = "Password must be at least 8 characters.";
+    return false;
+  }
+
+  const hasNumber = /\d/.test(password);
+  const hasSpecial = /[!]/.test(password);
+
+  if (!hasNumber || !hasSpecial) {
+    msg.textContent = "Password must include numbers and special characters.";
+    return false;
+  }
+
+  if (password !== confirm) {
+    msg.textContent = "Passwords do not match.";
+    return false;
+  }
+
+  alert("Success! Form submitted.");
+
+  return true;
+}
+
